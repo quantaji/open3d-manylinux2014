ac9abe1fd9c09ece4640deaaf46a7c63ba59fb7c
Guangda Ji <quanta.guangda.ji@gmail.com>
2023-09-08 23:56:28 +0200

    dd

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 export PATH=/opt/python/cp310-cp310/bin:$PATH
 
 cd /root/Open3D
+
+sed -i 's/Python3_FOUND/True/g' ./CMakeLists.txt
+
 mkdir build
 cd build
 cmake -DBUILD_EXAMPLES=OFF -DBUILD_FILAMENT_FROM_SOURCE=ON -DBUILD_GUI=OFF -DUSE_SYSTEM_GLFW=ON -DBUILD_WEBRTC=OFF ..
